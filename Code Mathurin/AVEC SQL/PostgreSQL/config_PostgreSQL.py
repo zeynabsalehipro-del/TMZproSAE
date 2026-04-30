cf0939300bd735e8421f38ebe084b8f57cab07e8
Mathurin-Martel <157318387+Mathurin-Martel@users.noreply.github.com>
2026-04-30 22:58:01 +0200

    Create config_PostgreSQL.py

---
+++ Code Mathurin/AVEC SQL/PostgreSQL/config_PostgreSQL.py
@@ -0,0 +1,13 @@
+DB_CONFIG = {
+    "host": "localhost",
+    "port": 5432,
+    "user": "postgres",
+    "password": "SQLroot",
+    "database": "TMZproSAE"
+}
+
+#Evite une connexion globale
+#Permet gestion par requête
+#Plus proche d'une architecture production
+
+#DOA=Data Object Access
